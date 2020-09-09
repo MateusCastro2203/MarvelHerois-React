98da57a7242d2ba0fb9678c175eafed8af8b93f7
Mateus Castro <mateustcastro@gmail.com>
2020-09-09 10:13:12 -0300

    finish

--- src/header/Header.js
+++ src/header/Header.js
@@ -4,16 +4,6 @@ import Herois from '../herois/herois.js'
 import './Header.css'
 import Request from '../request/Request.js'
 const Header = () => {
-    const keyPublic = '37f91eaebd4facebdc8035f7c753d62b';
-    const keyPrivate = 'a361d56ebc8828490953812d38eac23b263ddb2b';
-    var timestamp = new Date().getTime();
-    var md5 = Crypto.MD5(timestamp+keyPrivate+keyPublic);
-
-    /*function  share(){
-        var name = document.getElementById('nameHero')
-        Herois.shareHero(name);
-    }      
-    */
     return (
         <div id="header">
             <div id='logo'>
