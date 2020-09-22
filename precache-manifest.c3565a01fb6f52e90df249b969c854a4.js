9a5b97a601a342b3a038ee63db47d479573177b5
Mateus Castro <mateustcastro@gmail.com>
2020-09-22 18:57:55 -0300

    Updates

---
+++ precache-manifest.c3565a01fb6f52e90df249b969c854a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15c3c87f613032d7d8531e322ce4b08",
+    "url": "/Marvel-Hero/index.html"
+  },
+  {
+    "revision": "821eeebd67269b473c1f",
+    "url": "/Marvel-Hero/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d55042531b010b28da33",
+    "url": "/Marvel-Hero/static/css/main.43013be8.chunk.css"
+  },
+  {
+    "revision": "821eeebd67269b473c1f",
+    "url": "/Marvel-Hero/static/js/2.027eaba0.chunk.js"
+  },
+  {
+    "revision": "e2d9120e68abdb8c34ccfdcee3fe6c5a",
+    "url": "/Marvel-Hero/static/js/2.027eaba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d55042531b010b28da33",
+    "url": "/Marvel-Hero/static/js/main.e991c89e.chunk.js"
+  },
+  {
+    "revision": "03a5e009a753fc301ffb",
+    "url": "/Marvel-Hero/static/js/runtime-main.e1fa4ee2.js"
+  },
+  {
+    "revision": "01bd3ccacea9b68eb76f9c9fe898437a",
+    "url": "/Marvel-Hero/static/media/marvel.bold.01bd3cca.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9417d38b1055bbbd312969849f4f236c.js"
+  "/Marvel-Hero/precache-manifest.c3565a01fb6f52e90df249b969c854a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Marvel-Hero/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
